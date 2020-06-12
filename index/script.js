a2ca8ed925c6b05f16f655d59a6532a98ac52c8a
joooons <joon.s.chang@gmail.com>
2020-06-11 17:01:48 -0700

    menu and main initiated

---
+++ index/script.js
@@ -0,0 +1,57 @@
+console.log('itwork');
+
+{/* <i class="fas fa-envelope"></i> */}
+
+
+const main = document.querySelector('#main');
+const menu = document.querySelector('#menu');
+
+const icon4 = document.querySelector('i.fa-envelope');
+const menuToggle = document.querySelector('#menu-toggle');
+
+
+
+const menuWidth = {
+    min : '3em',
+    max : '200px'
+}
+
+
+
+// EVENT HANDLERS ________________________________________
+
+// main.onclick = () => { console.log(window.scrollY); }
+
+icon4.onclick = () => { window.scrollTo(0,300); }
+
+// document.onwheel = () => { console.log(window.scrollY); }
+
+menuToggle.onclick = () => {
+    menuWidthToggle();
+}
+
+window.onresize = () => {
+    adjustPageHeight();
+}
+
+
+// FUNCTIONS ____________________________________
+
+menuWidthToggle();
+
+function menuWidthToggle() {
+    let a = main.style.paddingLeft;
+    let b = menu.style.width;
+    // console.log(a,b);
+    main.style.paddingLeft = (a == menuWidth.max) ? menuWidth.min : menuWidth.max;
+    menu.style.width = (b == menuWidth.max) ? menuWidth.min : menuWidth.max;
+}
+
+
+
+adjustPageHeight();
+
+function adjustPageHeight() {
+    document.querySelector('.container').style.height = window.innerHeight;
+    console.log('window innerheight is ', window.innerHeight);
+}
