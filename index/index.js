40175fa9f1a0123687fa809f9de8d0f051b9a922
joooons <joon.s.chang@gmail.com>
2020-08-29 16:43:50 -0700

    menubar animation kinda works

--- index/index.js
+++ index/index.js
@@ -4,24 +4,44 @@ $(document).ready(function() {
 
 console.log('index.js at your service!');
 
-const bars = document.getElementById('bars');
+
+const bar = document.getElementById('bar');
 const menu = document.getElementById('menu');
+const close = document.getElementById('close');
 
 
 
 
 bars.onclick = () => {
-    if (menu.classList.contains('hello')) { menu.classList.remove('hello'); } 
-    else { menu.classList.add('hello'); }
+    if (menu.classList.contains('hello')) { 
+        menu.classList.remove('hello'); 
+        $(bar).delay(1000).fadeIn();
+    } 
+    else { 
+        $(bar).fadeOut(400, function() {
+            menu.classList.add('hello'); 
+        });
+    }
 }
 
+close.onclick = () => {
+    if (menu.classList.contains('hello')) { 
+        menu.classList.remove('hello'); 
+        $(bar).delay(1000).fadeIn();
+    } 
+    else { 
+        $(bar).fadeOut(400, function() {
+            menu.classList.add('hello'); 
+        });
+    }
+}
 
 
 
 
 });
 
-
+// Why? Cuz I'm lazy...
 function say(str) { console.log(str); }
 function tab(obj) { console.table(obj); }
 
