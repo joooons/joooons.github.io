11f1603efc6fdf7289d939c66b344a63c9b4e9b5
joooons <joon.s.chang@gmail.com>
2020-08-29 15:51:16 -0700

    menu slides in and out

--- index/index.js
+++ index/index.js
@@ -1,3 +1,32 @@
 
 
+$(document).ready(function() {
+
 console.log('index.js at your service!');
+
+const bars = document.getElementById('bars');
+const menu = document.getElementById('menu');
+
+
+
+
+bars.onclick = () => {
+    if (menu.classList.contains('hello')) { menu.classList.remove('hello'); } 
+    else { menu.classList.add('hello'); }
+}
+
+
+
+
+
+});
+
+
+function say(str) { console.log(str); }
+function tab(obj) { console.table(obj); }
+
+
+
+
+
+
