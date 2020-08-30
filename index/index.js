f4a75f88e73a631f889e9bd6ec1c6a8000cbe16f
joooons <joon.s.chang@gmail.com>
2020-08-29 21:38:44 -0700

    github icons

--- index/index.js
+++ index/index.js
@@ -8,33 +8,41 @@ console.log('index.js at your service!');
 const bar = document.getElementById('bar');
 const menu = document.getElementById('menu');
 const close = document.getElementById('close');
+const menuItem = document.getElementsByClassName('menu-item');
 
 
 
 
 bars.onclick = () => {
-    if (menu.classList.contains('hello')) { 
-        menu.classList.remove('hello'); 
-        $(bar).delay(1000).fadeIn();
-    } 
-    else { 
-        $(bar).fadeOut(400, function() {
-            menu.classList.add('hello'); 
-        });
-    }
+    if (menu.classList.contains('hello')) { menulist.close(); } 
+    else { menulist.open(); }
 }
 
 close.onclick = () => {
-    if (menu.classList.contains('hello')) { 
-        menu.classList.remove('hello'); 
-        $(bar).delay(1000).fadeIn();
-    } 
-    else { 
-        $(bar).fadeOut(400, function() {
-            menu.classList.add('hello'); 
-        });
-    }
+    if (menu.classList.contains('hello')) { menulist.close(); } 
+    else { menulist.open(); }
 }
+
+
+for ( i=0 ; i<menuItem.length ; i++ ) {
+    menuItem[i].onclick = () => { menulist.close(); }
+}
+
+
+
+
+const menulist = {
+    open : function() {
+            $(bar).fadeOut(200, function() { menu.classList.add('hello'); });
+            document.body.classList.add('noscroll');
+        },
+    close : function() {
+            menu.classList.remove('hello'); 
+            $(bar).delay(300).fadeIn(200);
+            document.body.classList.remove('noscroll');
+        }
+}
+
 
 
 
