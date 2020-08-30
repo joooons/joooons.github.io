72b446accc16c18150f44952b33017e4dbbc2f5a
joooons <joon.s.chang@gmail.com>
2020-08-30 10:06:23 -0700

    each foreground has top/bot lid

--- index/index.js
+++ index/index.js
@@ -9,6 +9,9 @@ const bar = document.getElementById('bar');
 const menu = document.getElementById('menu');
 const close = document.getElementById('close');
 const menuItem = document.getElementsByClassName('menu-item');
+const foreground = document.getElementsByClassName('foreground');
+const topLid = document.getElementsByClassName('top-lid');
+const botLid = document.getElementsByClassName('bot-lid');
 
 
 
@@ -23,10 +26,29 @@ close.onclick = () => {
     else { menulist.open(); }
 }
 
-
 for ( i=0 ; i<menuItem.length ; i++ ) {
     menuItem[i].onclick = () => { menulist.close(); }
 }
+
+
+
+
+for ( i=0 ; i<foreground.length ; i++ ) {
+    let num = i;
+    foreground[num].onmouseover = () => {
+        topLid[num].classList.add('show');
+        botLid[num].classList.add('show');
+    }
+}
+
+for ( i=0 ; i<foreground.length ; i++ ) {
+    let num = i;
+    foreground[i].onmouseout = () => {
+        topLid[num].classList.remove('show');
+        botLid[num].classList.remove('show');
+    }
+}
+
 
 
 
